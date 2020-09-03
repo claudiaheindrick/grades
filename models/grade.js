c370d44f415e71b88600b7e063a88a87e996cc50
claudiaheindrick <claudia.heindrickson@gmail.com>
2020-09-03 20:14:49 -0300

    first commit

---
+++ models/grade.js
@@ -0,0 +1,30 @@
+import mongoose from 'mongoose';
+
+const gradeSchema = mongoose.Schema({
+  name: {
+    type: String,
+    require: true,
+  },
+  subject: {
+    type: String,
+    require: true,
+  },
+  type: {
+    type: String,
+    require: true,
+  },
+  value: {
+    type: Number,
+    require: true,
+    min: 0,
+  },
+  lastModified: {
+    type: Date,
+    require: true,
+    default: Date.now,
+  },
+});
+
+const gradeModel = mongoose.model('grade', gradeSchema);
+
+export { gradeModel as model };
