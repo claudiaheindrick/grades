dcbc7e89fd58428dd6893192355e156089f099ee
claudiaheindrick <claudia.heindrickson@gmail.com>
2020-09-04 22:38:36 -0300

    route config

--- routes/grade.js
+++ routes/grade.js
@@ -1,7 +1,18 @@
 import express from 'express';
 import controller from '../controllers/grade.js';
+import cors from 'cors';
 
 const app = express();
+
+app.use((req, res, next) => {
+  res.header(
+    'Access-Control-Allow-Origin',
+    'https://claudiaheindrick-grades-app.herokuapp.com'
+  );
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  app.use(cors());
+  next();
+});
 
 app.post('/grade', controller.create);
 app.get('/grade', controller.findAll);
